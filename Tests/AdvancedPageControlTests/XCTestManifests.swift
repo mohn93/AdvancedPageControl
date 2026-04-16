3049aada492aa07a83789e4ac7eaf577e3ca43ff
Mohanned Binmiskeen <mohn93@gmail.com>
2026-04-16 11:34:49 +0200

    chore: update XCTestManifests and LinuxMain with all test classes

--- Tests/AdvancedPageControlTests/XCTestManifests.swift
+++ Tests/AdvancedPageControlTests/XCTestManifests.swift
@@ -4,6 +4,11 @@ import XCTest
 public func allTests() -> [XCTestCaseEntry] {
     return [
         testCase(DrawerParentTests.allTests),
+        testCase(DrawerParentWithIndicatorTests.allTests),
+        testCase(ColorUtilsTests.allTests),
+        testCase(AllDrawersTests.allTests),
+        testCase(ExtendedDotDrawerTests.allTests),
+        testCase(AdvancedPageControlViewTests.allTests),
     ]
 }
 #endif
--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -3,4 +3,9 @@ import XCTest
 
 XCTMain([
     testCase(DrawerParentTests.allTests),
+    testCase(DrawerParentWithIndicatorTests.allTests),
+    testCase(ColorUtilsTests.allTests),
+    testCase(AllDrawersTests.allTests),
+    testCase(ExtendedDotDrawerTests.allTests),
+    testCase(AdvancedPageControlViewTests.allTests),
 ])
